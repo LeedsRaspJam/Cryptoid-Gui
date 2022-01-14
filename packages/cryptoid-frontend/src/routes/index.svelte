ae92f7208be6ca3717f2bc3f79a77a38f53aec0b
James Cook <james@jaminit.co.uk>
2022-01-14 17:34:23 +0000

    fix: use if browser

--- packages/cryptoid-frontend/src/routes/index.svelte
+++ packages/cryptoid-frontend/src/routes/index.svelte
@@ -1,18 +1,18 @@
 <script>
 	import { connect, websocketMessages } from '../lib/websocket';
 	import { getCpuUsage, cpuUsage } from '../lib/data';
-	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
 	let websocketMessage;
-	onMount(() => {
-		 connect('ws://127.0.0.1:8080');
+	if (browser) {
+		connect('ws://127.0.0.1:8080');
 		websocketMessage = websocketMessages;
-	});
+	}
 </script>
 
 <body>
-		<p>Websocket</p>
-		<p>{$websocketMessage}</p>
-		<button on:click={getCpuUsage}>Get CPU</button>
-		<p>CPU count: {$cpuUsage?.cpuCount || 'N/A'}</p>
-		<p>CPU usage: {$cpuUsage?.cpuUsage || 'N/A'}</p>
+	<p>Websocket</p>
+	<p>{$websocketMessage}</p>
+	<button on:click={getCpuUsage}>Get CPU</button>
+	<p>CPU count: {$cpuUsage?.cpuCount || 'N/A'}</p>
+	<p>CPU usage: {$cpuUsage?.cpuUsage || 'N/A'}</p>
 </body>
